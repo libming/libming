e90a8c15d4ece12c9f2bbc928f24931a6569ffc1
opaque_ndst <opaque_ndst>
2001-03-12 20:54:34 +0000

    added Ming_setScale, moved Ming_setCubicThreshold here

--- src/ming.c
+++ src/ming.c
@@ -4,12 +4,25 @@
 /* module-wide initialization.  return non-zero if error. */
 
 extern int SWF_gNumCharacters;
-extern int Ming_cubicThreshold;
+
+float Ming_scale = 20.0;
+int Ming_cubicThreshold = 10000;
 
 int Ming_init()
 {
   SWF_gNumCharacters = 0;
   Ming_cubicThreshold = 10000;
+  Ming_scale = 20.0;
 
   return 0;
+}
+
+void Ming_setScale(float scale)
+{
+  Ming_scale = scale;
+}
+
+void Ming_setCubicThreshold(int num)
+{
+  Ming_cubicThreshold = num;
 }
