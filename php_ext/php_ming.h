a2ed5d35858fd3413e2f4817cfe009fd651c8ca5
opaque_ndst <opaque_ndst>
2001-03-12 20:58:45 +0000

    added Ming_setScale, changed scalar args to floats

--- php_ext/php_ming.h
+++ php_ext/php_ming.h
@@ -168,13 +168,13 @@ PHP_FUNCTION(swfbutton_addAction);
 PHP_FUNCTION(swfbutton_keypress);
 
 PHP_FUNCTION(swfaction_init);
-PHP_FUNCTION(swfaction_add);
 
 PHP_FUNCTION(swfmorph_init);
 PHP_FUNCTION(swfmorph_getShape1);
 PHP_FUNCTION(swfmorph_getShape2);
 
 PHP_FUNCTION(ming_setCubicThreshold);
+PHP_FUNCTION(ming_setScale);
 
 #else
 #define ming_module_ptr NULL
