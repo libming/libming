24eec983932bfdfb70cc78a1a99e5e86fc380050
soheil <soheil>
2001-09-24 01:02:44 +0000

    - added version - Moved :Text to Constants

--- perl_ext/SWF/TextField.pm
+++ perl_ext/SWF/TextField.pm
@@ -1,5 +1,5 @@
 # ====================================================================
-# Copyright (c) 2000 by Soheil Seyfaie. All rights reserved.
+# Copyright (c) 2000-2001 by Soheil Seyfaie. All rights reserved.
 # This program is free software; you can redistribute it and/or modify
 # it under the same terms as Perl itself.
 # ====================================================================
@@ -9,6 +9,8 @@
 
 package SWF::TextField;
 use SWF ();
+
+$SWF::TextField::VERSION = $SWF::VERSION;
 
 1;
 
@@ -20,41 +22,18 @@ SWF::TextField - SWF TextField class
 
 =head1 SYNOPSIS
 
- use SWF::TextField();   # Do not import any constant
- my $testfield = new SWF::TextField();
-
- #import all constants and functions available to current package
- use SWF::TextField qw(:Text);
-
+ use SWF::TextField;
 
 =head1 DESCRIPTION
 
-Creates a TextField for flash movies. By default, SWF::TextField will not import anything to your package, i.e. you have to explicitly indicate which constants you need. Here is the list of constants that you may import to your package:
+Creates a TextField for flash movies.
 
-    SWFTEXTFIELD_NOEDIT
-    SWFTEXTFIELD_PASSWORD
-    SWFTEXTFIELD_DRAWBOX
-    SWFTEXTFIELD_MULTILINE
-    SWFTEXTFIELD_WORDWRAP
-    SWFTEXTFIELD_NOSELECT
-    SWFTEXTFIELD_ALIGN_LEFT
-    SWFTEXTFIELD_ALIGN_RIGHT
-    SWFTEXTFIELD_ALIGN_CENTER
-    SWFTEXTFIELD_ALIGN_JUSTIFY
-    SWFTEXTFIELD_HTML
-    SWFTEXTFIELD_HASLENGTH
-
-For example, if you want to import SWFTEXTFIELD_HTM aonly, use the following statement:
-
-    use SWF::TextFiels qw(SWFTEXTFIELD_HTM);
-
-You may use the following shortcut, if you want to import everything
-
-    use SWF::TextField ':SWFTEXTFIELD_HTM';
 
 =head1 METHODS
 
-=item XXXXXXXXXXXXXXXXXXXXXX add methods
+=item  add methods here
+
+TODO
 
 =back
 
