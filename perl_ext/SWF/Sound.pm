e970200dcbdeea97cac94f8256976879b6e96933
soheil <soheil>
2001-09-24 02:04:20 +0000

    added version

--- perl_ext/SWF/Sound.pm
+++ perl_ext/SWF/Sound.pm
@@ -1,5 +1,5 @@
 # ====================================================================
-# Copyright (c) 2000 by Soheil Seyfaie. All rights reserved.
+# Copyright (c) 2000-2001 by Soheil Seyfaie. All rights reserved.
 # This program is free software; you can redistribute it and/or modify
 # it under the same terms as Perl itself.
 # ====================================================================
@@ -12,5 +12,6 @@ use SWF();
 
 use strict;
 
+$SWF::Sound::VERSION = $SWF::VERSION;
 
 1;
