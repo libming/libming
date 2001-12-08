51182e72fb0b3bff3e8e34af764cf3538a98697d
erich <erich>
2001-12-08 22:52:48 +0000

    Reported by Raymond Penners, inconsistency to ming.h in addString

--- mingpp.h
+++ mingpp.h
@@ -639,7 +639,7 @@ class SWFText : public SWFCharacter
   void setColor(unsigned char r, unsigned char g, unsigned char b, int a=0xff)
     { SWFText_setColor(this->text, r, g, b, a); }
 
-  void addString(const char *string, float *advance=NULL)
+  void addString(const char *string, int *advance=NULL)
     { SWFText_addString(this->text, string, advance); }
 
   void setSpacing(float spacing)
