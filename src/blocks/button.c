8a341d43f9e3368534ee148910d274c35429b460
opaque_ndst <opaque_ndst>
2002-08-07 23:30:31 +0000

    added Goto's button sound code (untested)

--- src/blocks/button.c
+++ src/blocks/button.c
@@ -1,6 +1,6 @@
 /*
     Ming, an SWF output library
-    Copyright (C) 2001  Opaque Industries - http://www.opaque.net/
+    Copyright (C) 2002  Opaque Industries - http://www.opaque.net/
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
@@ -56,10 +56,10 @@ struct SWFButton_s
 
 struct SWFButtonSound_s
 {
-  struct SWFBlock_s character;
+	struct SWFBlock_s character;
 
 	SWFButton button;
-  SWFSoundInstance sounds[4];
+	SWFSoundInstance sounds[4];
 };
 
 
@@ -81,7 +81,7 @@ static ButtonRecord newSWFButtonRecord(byte flags, SWFCharacter character,
 
 static void SWFButton_addRecord(SWFButton button, ButtonRecord record)
 {
-	if(button->nRecords % BUTTONRECORD_INCREMENT == 0)
+	if ( button->nRecords % BUTTONRECORD_INCREMENT == 0 )
 	{
 		button->records = realloc(button->records,
 						(button->nRecords + BUTTONRECORD_INCREMENT) *
@@ -90,14 +90,18 @@ static void SWFButton_addRecord(SWFButton button, ButtonRecord record)
 
 	button->records[button->nRecords++] = record;
 
-	SWFCharacter_addDependency((SWFCharacter)button, (SWFBlock)record->character);
+	SWFCharacter_addDependency((SWFCharacter)button,
+														 (SWFCharacter)record->character);
 }
 
 
 /* note: replaces action, doesn't append.. */
 void SWFButton_addAction(SWFButton button, SWFAction action, int flags)
 {
-	if(button->nActions % BUTTONRECORD_INCREMENT == 0)
+	if ( SWFCharacter_isFinished((SWFCharacter)button) )
+		SWF_error("Can't alter a button after it's been added to another character");
+
+	if ( button->nActions % BUTTONRECORD_INCREMENT == 0 )
 	{
 		button->actions = realloc(button->actions,
 						(button->nActions + BUTTONRECORD_INCREMENT) *
@@ -113,8 +117,14 @@ void SWFButton_addAction(SWFButton button, SWFAction action, int flags)
 /* XXX - temp hack */
 void SWFButton_addShape(SWFButton button, SWFCharacter character, byte flags)
 {
-	SWFMatrix m = newSWFMatrix(0, 0, 0, 0, 0, 0);
+	SWFMatrix m;
 
+	if ( SWFCharacter_isFinished((SWFCharacter)button) )
+		SWF_error("Can't alter a button after it's been added to another character");
+
+	m = newSWFMatrix(0, 0, 0, 0, 0, 0);
+
+	SWFCharacter_setFinished(character);
 	SWFButton_addRecord(button, newSWFButtonRecord(flags, character, 0, m));
 }
 
@@ -193,30 +203,31 @@ void destroySWFButton(SWFBlock block)
 	SWFButton button = (SWFButton)block;
 	int i;
 
-	SWFCharacter_clearDependencies(CHARACTER(block));
-
-	for(i=0; i<button->nRecords; ++i)
+	for ( i=0; i<button->nRecords; ++i )
 	{
-		if(button->records[i]->matrix != NULL)
+		if ( button->records[i]->matrix != NULL )
 			destroySWFMatrix(button->records[i]->matrix);
 
-		sec_free((void**)&button->records[i]);
+		free(button->records[i]);
 	}
 
-	sec_free((void**)&button->records);
+	if ( button->records != NULL )
+		free(button->records);
 
-	for(i=0; i<button->nActions; ++i)
+	for ( i=0; i<button->nActions; ++i )
 		destroySWFAction(button->actions[i].action);
-
-	sec_free((void**)&button->actions);
+	
+	if ( button->actions != NULL )
+		free(button->actions);
 
 	destroySWFOutput(button->out);
 
-	sec_free((void**)&button);
+	destroySWFCharacter(block);
 }
 
 
-SWFButton newSWFButton()
+SWFButton
+newSWFButton()
 {
 	SWFButton button = malloc(sizeof(struct SWFButton_s));
 
@@ -242,31 +253,38 @@ SWFButton newSWFButton()
 SWFSoundInstance
 SWFButton_addSound(SWFButton button, SWFSound sound, byte flag)
 {
+	if ( SWFCharacter_isFinished((SWFCharacter)button) )
+		SWF_error("Can't alter a button after it's been added to another character");
+
 	if ( button->sounds == NULL )
 	{
 		button->sounds = newSWFButtonSound(button);
-		SWFCharacter_addDependency((SWFCharacter)button, (SWFBlock)button->sounds);
+
+		SWFCharacter_addDependency((SWFCharacter)button,
+															 (SWFCharacter)button->sounds);
 	}
 
 	return SWFButtonSound_setSound(button->sounds, sound, flag);
 }
 
 
-void destroySWFButtonSound(SWFBlock buttonSound)
+void
+destroySWFButtonSound(SWFBlock buttonSound)
 {
-	sec_free((void**)&buttonSound);
+	free(buttonSound);
 }
 
 
-void writeSWFButtonSoundToMethod(SWFBlock block,
-																 SWFByteOutputMethod method, void *data)
+void
+writeSWFButtonSoundToMethod(SWFBlock block,
+														SWFByteOutputMethod method, void *data)
 {
 	SWFButtonSound buttonSound = (SWFButtonSound)block;
 	int i;
 
 	methodWriteUInt16(CHARACTERID(buttonSound->button), method, data);
 
-	for (i=0; i<4; ++i)
+	for ( i=0; i<4; ++i )
 	{
 		SWFSoundInstance sound = buttonSound->sounds[i];
 		writeSWFSoundInstanceToMethod((SWFBlock)sound, method, data);
@@ -274,13 +292,14 @@ void writeSWFButtonSoundToMethod(SWFBlock block,
 }
 
 
-int completeSWFButtonSound(SWFBlock block)
+int
+completeSWFButtonSound(SWFBlock block)
 {
 	int size = 2;	 /* button ID */
 	int i;
 	SWFButtonSound buttonSound = (SWFButtonSound)block;
 
-	for (i=0; i<4; ++i)
+	for ( i=0; i<4; ++i )
 	{
 		SWFSoundInstance sound = buttonSound->sounds[i];
 		size += completeSWFSoundInstance((SWFBlock)sound);
@@ -290,7 +309,8 @@ int completeSWFButtonSound(SWFBlock block)
 }
 
 
-SWFButtonSound newSWFButtonSound(SWFButton button)
+SWFButtonSound
+newSWFButtonSound(SWFButton button)
 {
 	SWFButtonSound buttonSound = malloc(sizeof(struct SWFButtonSound_s));
 	SWFBlock block = (SWFBlock)buttonSound;
