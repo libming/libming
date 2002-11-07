a1eb01682e9ada223c856123776a1cbb4549577e
peterdd <peterdd>
2002-11-07 12:39:33 +0000

    typofixes

--- perl_ext/SWF/TextField.pm
+++ perl_ext/SWF/TextField.pm
@@ -72,24 +72,40 @@ Set an initial string.
 
 	$tf->addString("blablabla"); 
 
+
+Notice: ming-API currently(CVS Oct.2002) in unstable definition. Maybe 
+addUTF8() or addWideString(). Try what works in your version of ming.
+
+=item	setAlignment(SWF::Constants)
+
+Set the alignment of the text in the textfield. Possible values are
+
+	SWFTEXTFIELD_ALIGN_LEFT
+	SWFTEXTFIELD_ALIGN_RIGHT
+	SWFTEXTFIELD_ALIGN_CENTER
+	SWFTEXTFIELD_ALIGN_JUSTIFY
+	
+By default a SWF::TextField ist left aligned.
+	
+	$tf->setAlignment(SWFTEXTFIELD_RIGHT);
+
 =item	setBounds(width,height)
 
 Set the width and height of the textfield.
-
+	
 	$tf->setBounds(50,40);
 
 =item setColor(red,green,blue,[alpha])
 
 Set the color of the text. An 8 bit value for each parameter. You can use 
-the hex or dezimal notation. Even mixed.
-
+the hex or decimal notation. Even mixed.
+	
 	$tf->setColor(0xcc,0,0x33);
 	$tf->setColor(204,0,51,255);
 
 =item setFieldHeight(height)
 
 Set the field height? Combine it with $f->getHeight();
-
 
 =item setFont(SWF::Font)
 
@@ -106,7 +122,7 @@ spacecharacter :-).
 
 =item setHeight(height)
 
-Set the height of font in your texfield to 14 pixels.
+Set the height of font in your texfield in pixels.
 
 	$tf->setHeight(14);
 
@@ -156,53 +172,52 @@ $string is the name of a variable.
 
 =head1 EXAMPLE
 
-Two textfields. First shows a changing actionscriptvariable, in the 
-second we put a Perlvariable as string into the textfield.
-
+Two textfields. First shows a changing Actionscript-variable, in the 
+second we put a Perl-variable as string into the textfield.
+	
 	#!/usr/bin/perl -w
 	use SWF("Text");
 	use SWF::Constants(":Text");
 	use English; # $OSNAME instead of cryptic $^O 
-
+	 
 	$m = new SWF::Movie();
 	$m->setDimension(200,100);
-
+	 
 	$f = new SWF::Font("_sans.fdb"); #good, _sans is a good default font. 
-
-        $tf1 = new SWF::TextField(SWFTEXTFIELD_DRAWBOX | SWFTEXTFIELD_WORDWRAP);
-        $tf1->setFont($f);
-        $tf1->addString("whoaw - a string. :-)"); # here useless but possible
-        $tf1->setHeight(14); # font size, default value: ??
-        $tf1->setBounds(70,20); # default value: ??
-        $tf1->setName("_root.currentFrame"); # Actionscript variable	
-
-	$tf2 = new SWF::TextField(SWFTEXTFIELD_DRAWBOX | SWFTEXTFIELD_NOEDIT);
+	 
+	$tf1 = new SWF::TextField(SWFTEXTFIELD_DRAWBOX | SWFTEXTFIELD_WORDWRAP);
+	$tf1->setFont($f);
+	$tf1->addString("whoaw - a string. :-)"); # here useless but possible
+	$tf1->setHeight(14); # font size, default value??
+	$tf1->setBounds(70,20); # default values??
+	$tf1->setName("_root.currentFrame"); # Actionscript variable	
+	 
+	tf2 = new SWF::TextField(SWFTEXTFIELD_DRAWBOX | SWFTEXTFIELD_NOEDIT);
 	$tf2->setFont($f);
-	$tf2->setHeight(11.25); # font size, default value: ??
-	$tf2->setBounds(50,14); # default value: ??
-	$tf2->addString($OSNAME); # put the systems name into the field
-
-	# put our 2 textfield in the movie
+	$tf2->setHeight(11.25);
+	$tf2->setBounds(50,14);
+	$tf2->addString($OSNAME); # We put the systems name into the field.
+	 
+	# We put our 2 textfields in the movie.
 	$tf1_i= $m->add($tf1);
 	$tf2_i= $m->add($tf2);
 	$tf1_i->move(20,20);
 	$tf2_i->move(20,50);
-
+	 
 	# make 10 frame
 	for ($i=1;$i<=10;$i++){
 		$m->nextFrame();
 	}
-
 	$m->save("textfield.swf");
 
 =head1 AUTHOR
 
-Soheil Seyfaie (soheil@users.sourceforge.net)
+Soheil Seyfaie (soheil at users.sourceforge.net)
 
-Documentation: Peter Liscovius (peterdd@users.sourceforge.net)
+Documentation: Peter Liscovius (peterdd at users.sourceforge.net)
 
 =head1 SEE ALSO
 
-	SWF:Constants, SWF::Font, SWF::Movie
+	SWF::Constants, SWF::Font, SWF::Action
 
 =cut
