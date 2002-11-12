c19c8ffab62e48e293d56be6375f808cd30dfaa0
zebulon75018 <zebulon75018>
2002-11-12 23:15:35 +0000

    First commit for the tcl extension After making the mingc.so try tclsh test.tcl , it's the shape example.

---
+++ tcl_ext/test.tcl
@@ -0,0 +1,15 @@
+load ./mingc.so mingc
+set shape [ newSWFShape ]
+SWFShape_setLine $shape 4 100 0 0 255
+SWFShape_setRightFill $shape [ SWFShape_addSolidFill $shape 255 0 0 255 ]
+SWFShape_movePenTo $shape 10 10
+SWFShape_drawLineTo $shape 310 10
+SWFShape_drawLineTo $shape 310 230
+SWFShape_drawCurveTo $shape 10 230 10 10
+
+set movie [newSWFMovie  ]
+SWFMovie_setDimension $movie  320  240 
+SWFMovie_add $movie $shape 
+SWFMovie_nextFrame $movie
+SWFMovie_save $movie "test.swf"
+
