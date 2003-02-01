a6eeb0559de9c797e7a0ab8803e2b1c899d05340
soheil <soheil>
2003-02-01 18:30:19 +0000

    Added getPen() documentation

--- perl_ext/SWF/Shape.pm
+++ perl_ext/SWF/Shape.pm
@@ -1,10 +1,9 @@
 # ====================================================================
-# Copyright (c) 2000-2001 by Soheil Seyfaie. All rights reserved.
+# Copyright (c) 2000-2003 by Soheil Seyfaie. All rights reserved.
 # This program is free software; you can redistribute it and/or modify
 # it under the same terms as Perl itself.
 # ====================================================================
 
-# $Author$
 # $Id$
 
 package SWF::Shape;
@@ -68,6 +67,18 @@ Move the Shape's (imaginary) pen location to ($x, $y) in the Shape's coordinate 
 =item $shape->moveToRelative($dx, $dy); or $shape->movePen($dx, $dy);
 
 Add ($dx, $dy) to the Shape's pen current location.
+
+=item $x = $shape->getPenX();
+
+Returns Pen's x-coordinates
+
+=item $y = $shape->getPenY();
+
+Returns Pen's y-coordinates
+
+=item ($x, $y) = $shape->getPen();
+
+Returns Pen's (x,y) coordinates
 
 =item $shape->drawLineTo($x, $y);
 
@@ -149,11 +160,10 @@ Draws a circle with radious $r.
 
 =head1 AUTHOR
 
-Soheil Seyfaie (soheil@netcom.ca).
+Soheil Seyfaie (soheil at users.sourceforge.net).
 
 =head1 SEE ALSO
 
-SWF, SWF::Action, SWF::Button, SWF::Movie, .
-
+SWF, SWF::Action, SWF::Bitmap, SWF::Button, SWF::Constants, SWF::DisplayItem, SWF::Fill, SWF::Font, SWF::Gradient, SWF::Morph, SWF::Movie, SWF::Shape, SWF::Sound, SWF::Sprite, SWF::TextField, SWF::Text
 
 =cut
