afa7880839323388d8288ee5d15ad291edac4251
whamann <whamann>
2005-03-03 19:07:28 +0000

    wrong call parameter in previous fix -- oops

--- src/blocks/fromswf.c
+++ src/blocks/fromswf.c
@@ -758,7 +758,7 @@ static void morphfillstyle(TAG tp)
 	}
 	else if(cod == 0x10 || cod == 0x12)
 	{	matrix((BITS) tp);
-		alignbits();
+		alignbits(tp);
 		matrix((BITS) tp);
 		morphgradient(tp);
 	}
@@ -766,7 +766,7 @@ static void morphfillstyle(TAG tp)
 	{	id = change_id(tp);
 		if(verbose) printf("fill with id %d\n", id);
 		matrix((BITS) tp);
-		alignbits();
+		alignbits(tp);
 		matrix((BITS) tp);
 	}
 	else
