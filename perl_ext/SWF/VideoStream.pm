1906f04a974ad6f050a9f87bc69616df98320e63
peterdd <peterdd>
2006-01-25 07:14:39 +0000

    added stub for documentation of VideoStream

---
+++ perl_ext/SWF/VideoStream.pm
@@ -0,0 +1,44 @@
+# ====================================================================
+# Copyright (c) 2006 by Peter Liscovius. All rights reserved.
+# This program is free software; you can redistribute it and/or modify
+# it under the same terms as Perl itself.
+# ====================================================================
+
+# $Author$
+# $Id$
+
+package SWF::VideoStream;
+use SWF();
+
+use strict;
+
+$SWF::Sound::VERSION = $SWF::VERSION;
+
+1;
+
+__END__
+
+=head1 NAME
+
+SWF::VideoStream - SWF Video class
+
+=head1 SYNOPSIS
+
+	use SWF::VideoStream;
+	$videostream = new SWF::VideoStream("test.flv");
+
+
+=head1 DESCRIPTION
+
+	XXX
+
+=head1 AUTHOR
+
+	developers of 
+	ming.sourceforge.net
+
+=head1 SEE ALSO
+
+SWF, SWF::Action, SWF::Bitmap, SWF::Button, SWF::Constants, SWF::DisplayItem, SWF::Fill, SWF::Font, SWF::Gradient, SWF::Morph, SWF::Movie, SWF::Shape, SWF::Sound, SWF::Sprite, SWF::TextField, SWF::Text
+
+=cut
