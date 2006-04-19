e8fac64672eeb51fa6aec388036420a3f0dcc61d
vapour <vapour>
2006-04-19 13:15:13 +0000

    Automatically detect the presence of getopt and getopt_long functions.

--- util/makeswf.h
+++ util/makeswf.h
@@ -1,8 +1,8 @@
 /* Define this if you have getopt.h */
-#undef HAVE_GETOPT_H
+//#undef HAVE_GETOPT_H
 
 /* Define this if you have getopt_long */
-#undef HAVE_GETOPT_LONG
+//#undef HAVE_GETOPT_LONG
 
 /* Set this to the default include path */
 #define DEFAULT_FLAGS "-I/usr/share/actionscript -I/usr/local/share/actionscript -I."
