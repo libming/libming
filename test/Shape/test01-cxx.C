1b2a9ee8a59db393fd4e6b1ab35f774949b1ab8d
krechert <krechert>
2007-04-22 17:30:13 +0000

    simple shape testcases

---
+++ test/Shape/test01-cxx.C
@@ -0,0 +1,12 @@
+#include <mingpp.h>
+
+int main()
+{
+	SWFMovie *m = new SWFMovie(8);
+	SWFShape *shape1 = new SWFShape();
+	SWFShape *shape2 = new SWFShape();
+	shape2->useVersion(SWF_SHAPE4);
+	m->add(shape1);
+	m->add(shape2);
+	m->save("test01.swf");
+}
---
+++ test/Shape/test01.c
@@ -0,0 +1,12 @@
+#include <libming.h>
+
+int main()
+{
+	SWFMovie m = newSWFMovieWithVersion(8);
+	SWFShape shape1 = newSWFShape();
+	SWFShape shape2 = newSWFShape();
+	SWFShape_useVersion(shape2, SWF_SHAPE4);
+	SWFMovie_add(m, (SWFBlock)shape1);
+	SWFMovie_add(m, (SWFBlock)shape2);
+	SWFMovie_save(m, "test01.swf");
+}
