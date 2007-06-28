25901c142a0461af436b2abed01fc8b715afbcc4
krechert <krechert>
2007-06-28 21:40:05 +0000

    png fill test

---
+++ test/FillStyle/test05-cxx.C
@@ -0,0 +1,23 @@
+#include <mingpp.h>
+
+int main()
+{
+	SWFMovie *m = new SWFMovie(8);
+	SWFBitmap *bmp = new SWFBitmap("../Media/image01.png");
+
+
+	SWFFillStyle *fill =  SWFFillStyle::BitmapFillStyle(bmp, SWFFILL_CLIPPED_BITMAP);
+	SWFShape *shape = new SWFShape();
+	shape->setRightFillStyle(fill);
+	
+	shape->setLine(1, 0,0,0,255);
+	shape->drawLine(100, 0);
+	shape->drawLine(0, 100);
+	shape->drawLine(-100, 0);
+	shape->drawLine(0, -100);
+
+	m->add(shape);
+	m->save("test05.swf");
+	return 0;
+
+}
---
+++ test/FillStyle/test05.c
@@ -0,0 +1,22 @@
+#include <libming.h>
+
+int main()
+{
+	SWFMovie m = newSWFMovieWithVersion(8);
+	SWFDBLBitmapData img = newSWFDBLBitmapData_fromPngFile("../Media/image01.png");
+	
+	SWFFillStyle fill = newSWFBitmapFillStyle((SWFCharacter)img, SWFFILL_CLIPPED_BITMAP);
+	SWFShape shape = newSWFShape();
+	SWFShape_setRightFillStyle(shape, fill);
+	
+	SWFShape_setLine(shape, 1, 0,0,0,255);
+	SWFShape_drawLine(shape, 100, 0);
+	SWFShape_drawLine(shape, 0, 100);
+	SWFShape_drawLine(shape, -100, 0);
+	SWFShape_drawLine(shape, 0, -100);
+
+	SWFMovie_add(m, (SWFBlock)shape);
+	SWFMovie_save(m, "test05.swf");
+	return 0;
+
+}
