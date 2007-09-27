5dd9b6e5caff3ecf98b909008d5ffeb236898b50
krechert <krechert>
2007-09-27 19:08:19 +0000

    update c-testcases. all other bindings set bgColor

--- test/Movie/add/test01.c
+++ test/Movie/add/test01.c
@@ -8,7 +8,7 @@ main()
 SWFMovie m;
 
 m = newSWFMovie();
-
+SWFMovie_setBackground(m, 0xff, 0xff, 0xff);
 SWFMovie_add(m, (SWFBlock)compileSWFActionCode("var a = 1;"));
 
 SWFMovie_save(m, "test01.swf");
--- test/Movie/add/test02.c
+++ test/Movie/add/test02.c
@@ -10,7 +10,7 @@ SWFMovieClip mc;
 
 m = newSWFMovie();
 mc = newSWFMovieClip();
-
+SWFMovie_setBackground(m, 0xff, 0xff, 0xff);
 SWFMovie_add(m, (SWFBlock)mc);
 
 SWFMovie_save(m, "test02.swf");
