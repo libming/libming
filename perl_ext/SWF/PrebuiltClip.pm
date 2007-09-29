48287b6b148c7c08e3f3cb62895ad453118656b5
peterdd <peterdd>
2007-09-29 06:57:03 +0000

    stub for the PrebuiltClip module

---
+++ perl_ext/SWF/PrebuiltClip.pm
@@ -0,0 +1,18 @@
+package SWF::PrebuiltClip;
+use SWF ();
+
+$SWF::PrebuiltClip::VERSION = $SWF::VERSION;
+
+1;
+
+__END__
+
+=head1 NAME
+
+SWF::PrebuiltClip - SWF PrebuiltClip class
+
+=head1 SYNOPSIS
+
+use SWF::PrebuiltClip;
+my $clip = new SWF::PrebuiltClip("prebuilt.swf");
+ 
