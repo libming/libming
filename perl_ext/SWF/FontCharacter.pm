52c6ab2609515040dd379bf4968f869c5dc8177f
krechert <krechert>
2007-11-10 18:38:38 +0000

    add FontCharacter.pm

---
+++ perl_ext/SWF/FontCharacter.pm
@@ -0,0 +1,42 @@
+# ====================================================================
+# Copyright (c) 2006 by Peter Liscovius. All rights reserved.
+# This program is free software; you can redistribute it and/or modify
+# it under the same terms as Perl itself.
+# ====================================================================
+
+# $Author$
+# $Id$
+
+package SWF::FontCharacter;
+use SWF();
+
+use strict;
+
+$SWF::FontCharacter::VERSION = $SWF::VERSION;
+
+1;
+
+__END__
+
+=head1 NAME
+
+SWF::FontCharacter - SWF Font Helper Class
+
+=head1 SYNOPSIS
+
+
+
+=head1 DESCRIPTION
+
+	XXX
+
+=head1 AUTHOR
+
+	developers of ming 
+	ming.sourceforge.net
+
+=head1 SEE ALSO
+
+SWF, SWF::Movie 
+
+=cut
