d7c96e3c17b03a224af20ef33c47887ab73e17b4
krechert <krechert>
2007-11-23 17:38:33 +0000

    add gif testcase

---
+++ test/FillStyle/test06.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <libming.h>
+
+int main()
+{
+	SWFMovie m = newSWFMovieWithVersion(8);
+	SWFDBLBitmapData img = newSWFDBLBitmapData_fromGifFile(MEDIADIR "/image01.gif");
+	
+	SWFFillStyle fill = newSWFBitmapFillStyle((SWFCharacter)img, SWFFILL_CLIPPED_BITMAP);
+	SWFShape shape = newSWFShape();
+	SWFShape_setRightFillStyle(shape, fill);
+	
+	SWFShape_setLine(shape, 1, 0,0,0,255);
+	SWFShape_drawLine(shape, 100, 0);
+	SWFShape_drawLine(shape, 0, 100);
+	SWFShape_drawLine(shape, -100, 0);
+	SWFShape_drawLine(shape, 0, -100);
+
+	SWFMovie_add(m, (SWFBlock)shape);
+	SWFMovie_save(m, "test06.swf");
+	return 0;
+
+}
