3fd8cdb8d709aadad45ab24901f999056f208e73
krechert <krechert>
2007-11-30 16:00:34 +0000

    php video testcase: init class with filename

---
+++ test/Video/test01.php
@@ -0,0 +1,11 @@
+#!/usr/bin/php -c.
+<?php
+$m = new SWFMovie(7);
+
+$video = new SWFVideoStream("../Media/video01.flv");
+$video->setDimension(200, 200);
+$frames = $video->getNumFrames();
+for($i = 0; $i < $frames; $i++)
+	$m->nextFrame();
+$m->save("test01.swf");
+?>
