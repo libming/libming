2174c180868da2ef806e51297e3ff50be1ecea5b
krechert <krechert>
2008-02-12 17:00:25 +0000

    added error handling

--- test/Bitmap/test01-cxx.C
+++ test/Bitmap/test01-cxx.C
@@ -2,11 +2,18 @@
 
 int main()
 {
-	SWFMovie *m = new SWFMovie(8);
-	SWFBitmap *bmp = new SWFBitmap(MEDIADIR "/image01.png");
+	try {
+		SWFMovie *m = new SWFMovie(8);
+		SWFBitmap *bmp = new SWFBitmap(MEDIADIR "/image01.png");
 
-	m->add(bmp);
-	m->save("test01.swf");
+		m->add(bmp);
+		m->save("test01.swf");
+	}
+	catch (SWFException &e)
+	{
+		std::cerr << "SWFException: " << e.what() << std::endl << std::endl;
+		return EXIT_FAILURE;
+	}
 	return 0;
 
 }
--- test/Bitmap/test01.c
+++ test/Bitmap/test01.c
@@ -1,9 +1,12 @@
 #include <libming.h>
+#include <stdlib.h>
 
 int main()
 {
 	SWFMovie m = newSWFMovieWithVersion(8);
 	SWFDBLBitmapData img = newSWFDBLBitmapData_fromPngFile(MEDIADIR "/image01.png");
+	if(img == NULL)
+		return EXIT_FAILURE;
 	
 	SWFMovie_add(m, (SWFBlock)img);
 	SWFMovie_save(m, "test01.swf");
