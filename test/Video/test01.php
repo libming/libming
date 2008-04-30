e8e64a9e584d1025b158669ac8913f89f52fd471
krechert <krechert>
2008-04-30 15:47:38 +0000

    do not forget to add the videostream to the movie

--- test/Video/test01.php
+++ test/Video/test01.php
@@ -4,6 +4,7 @@ $m = new SWFMovie(7);
 
 $video = new SWFVideoStream("../Media/video01.flv");
 $video->setDimension(200, 200);
+$m->add($video);
 $frames = $video->getNumFrames();
 for($i = 0; $i < $frames; $i++)
 	$m->nextFrame();
