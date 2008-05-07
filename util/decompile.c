57c2dd98d1d8c38f9ccfdd8098405453d02c2544
krechert <krechert>
2008-05-07 15:15:05 +0000

    only print push-types in DEBUG-mode

--- util/decompile.c
+++ util/decompile.c
@@ -577,7 +577,9 @@ void
 push(struct SWF_ACTIONPUSHPARAM *val)
 {
 	struct _stack *t;
+#ifdef DEBUG
 	printf("*push* type=%d\n",val->Type);
+#endif
 	t = calloc(1,sizeof(*Stack));
 	t->type = val->Type;
 	t->val = val;
