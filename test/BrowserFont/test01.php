0deae50f622aca51d1c02a0527e1713e5ff962fc
krechert <krechert>
2008-06-23 09:14:48 +0000

    php testcase

---
+++ test/BrowserFont/test01.php
@@ -0,0 +1,19 @@
+#!/usr/bin/php -c.
+
+<? 
+
+$m = new SWFMovie();
+$tf = new SWFTextField();
+
+$font = new SWFFont("_sans");
+
+$tf->setFont($font);
+$tf->setColor(0, 0, 0);
+$tf->setHeight(20);
+$tf->addString("abc");
+
+$m->add($tf);
+$m->nextFrame();
+$m->save("test01.swf");
+
+?>
