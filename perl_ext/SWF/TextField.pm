5be2a20d1176bb183fb1cd218780b85f69c842d9
akleine <akleine>
2009-01-06 17:35:03 +0000

    pod update

--- perl_ext/SWF/TextField.pm
+++ perl_ext/SWF/TextField.pm
@@ -23,7 +23,7 @@ SWF::TextField - SWF TextField class
 =head1 SYNOPSIS
 
 	use SWF::TextField;
-	$textfield = new SWF::TextField([flags]);
+	$textfield = new SWF::TextField([$flags]);
 
 
 
@@ -41,7 +41,7 @@ Unlike SWF::Text objects, TextFields cannot be rotated, scaled non-proportionall
 
 =over
 
-=item new SWF::TextField([flags]);
+=item new SWF::TextField([$flags]);
 
 Creates a TextField object whose behaviour is dictated by C<flags> (see also SWF::Constants): 
 
@@ -68,9 +68,13 @@ Flags may be combined with the bitwise OR operation. For example:
 
 =item $textfield->addString($string)
 
+=item $textfield->addUTF8String($UTF8string)
+
 Add $string to TextField. Text is appended to the existing text.
 
-=item $textfield->align(alignment)
+=item $textfield->align($alignment)
+
+=item $textfield->setAlignment($alignment)
 
 Set the alignment of the text in the textfield. Possible values are
 
@@ -78,18 +82,18 @@ Set the alignment of the text in the textfield. Possible values are
 	SWFTEXTFIELD_ALIGN_RIGHT
 	SWFTEXTFIELD_ALIGN_CENTER
 	SWFTEXTFIELD_ALIGN_JUSTIFY
-	
+
 By default a SWF::TextField ist left aligned.
 
-=item $textfield->setBounds(width, height)
+=item $textfield->setBounds($width, $height)
 
 Sets the width and height of the textfield.
 
-=item $textfield->setColor(red, green, blue [, alpha])
+=item $textfield->setColor($red, $green, $blue [, $alpha])
 
 Set the color of the text. An 8 bit value for each parameter. You can use 
 the hex or decimal notation. Even mixed.
-	
+
 	$tf->setColor(0xcc,0,0x33);
 	$tf->setColor(204,0,51,255);
 
@@ -97,41 +101,59 @@ the hex or decimal notation. Even mixed.
 
 Sets an SWF::Font object -$font- to be used in the TextField.
 
-=item $textfield->setPadding(padding)
+=item $textfield->setPadding($padding)
 
 Set TextField padding.
 
-=item $textfield->setHeight(height)
+=item $textfield->setHeight($height)
 
 Set the height of font in your textfield.
 
-=item $textfield->setIndentation(indentation)
+=item $textfield->setIndentation($indentation)
 
 Set the indentation of the first line of a paragraph.
 
-=item $textfield->setLeftMargin(left)
+=item $textfield->setLeftMargin($left)
 
 Sets the left margin of the textfield.
 
-=item $textfield->setRightMargin(right)
+=item $textfield->setRightMargin($right)
 
 Sets the right margin of the textfield.
 
-=item $textfield->setMargins(left, right)
+=item $textfield->setMargins($left, $right)
 
 Sets left and right margins of $textfield.
 
-=item $textfield->setLineSpacing(linespacing)
+=item $textfield->setLineSpacing($linespacing)
 
 Sets the space between lines. 
 
 =item $textfield->setName($name)
 
+=item $textfield->setVariableName($name)
+
 Assigns a name to the TextField which could be used to reference the TextField within ActionScript.
 
-=item $textfield->addChars(string)
+=item $textfield->addChars($string)
 
+=item $textfield->addUTF8Chars($UTF8string)
 
+Adds characters to a font that will be available within a textfield.
+
+=item $textfield->setFlags($flags)
+
+Set TextField behaviour by C<flags> (see above new SWF::TextField) For example:
+
+	$textfield->setFlags(SWFTEXTFIELD_PASSWORD); 
+
+=item $textfield->setLength($length)
+
+Sets limit for user input size to $length.
+
+=item $textfield->setFieldHeight($height)
+
+Sets height limit for visible part of text field. If $height is set to 0, the height is calculated by used font, see I<setHeight()> method.
 
 =back
 
