e30415eb90a9f7ad0daee6a80b7b71ac4cf1b5d7
akleine <akleine>
2009-01-13 17:59:15 +0000

    added initial pod

--- perl_ext/SWF/Blur.pm
+++ perl_ext/SWF/Blur.pm
@@ -1,5 +1,6 @@
 # ====================================================================
 # Copyright (c) 2008 Klaus Rechert All rights reserved.
+#           (c) 2009 A.Kleine
 # This program is free software; you can redistribute it and/or modify
 # it under the same terms as Perl itself.
 # ====================================================================
@@ -11,31 +12,44 @@ package SWF::Blur;
 use SWF ();
 
 $SWF::Blur::VERSION = $SWF::VERSION;
+
 1;
 
 __END__
 
 =head1 NAME
 
-SWF::Matrix - SWF blur class
+SWF::Blur - SWF blur class
 
 =head1 SYNOPSIS
 
+	use SWF::Blur;
+	$blur = new SWF::Blur(15, 15, 2);
+	$filter = SWF::Filter::newDropShadowFilter( \@colors, $blur, $shadow, 0);
+
 =head1 DESCRIPTION
 
+SWF::Blur is a helper class useful for SWF::Filter.
 
 =head1 METHODS
 
-=over 4
+=over
 
+=item new SWF::Blur($blurX, $blurY, $passes)
+
+Creates a Blur object. The object is controled by the horizontal blur amount -blurX-, 
+the vertical blur amount -blurY- and the count of passes (use 1, 2 or 3).
+Use the created object as parameter for construction of some of the SWF::Filter objects.
 
 =back
 
 =head1 AUTHOR
 
+	developers of 
+	ming.sourceforge.net
 
 =head1 SEE ALSO
 
-SWF, .
+SWF, SWF::Filter, SWF::Blur, SWF::DisplayItem
 
 =cut
--- perl_ext/SWF/Shadow.pm
+++ perl_ext/SWF/Shadow.pm
@@ -1,5 +1,6 @@
 # ====================================================================
 # Copyright (c) 2008 Klaus Rechert All rights reserved.
+#           (c) 2009 A.Kleine
 # This program is free software; you can redistribute it and/or modify
 # it under the same terms as Perl itself.
 # ====================================================================
@@ -11,31 +12,46 @@ package SWF::Shadow;
 use SWF ();
 
 $SWF::Shadow::VERSION = $SWF::VERSION;
+
 1;
 
 __END__
 
 =head1 NAME
 
-SWF::Matrix - SWF shadow class
+SWF::Shadow - SWF shadow class
 
 =head1 SYNOPSIS
 
+	use SWF::Shadow;
+	$shadow = new SWF::Shadow(2.5, 12, 2);
+	$filter = SWF::Filter::newDropShadowFilter( \@colors, $blur, $shadow, 0);
+
 =head1 DESCRIPTION
 
+SWF::Shadow is a helper class useful for SWF::Filter.
 
 =head1 METHODS
 
-=over 4
+=over
 
+=item new SWF::Shadow($angle, $distance, $strength)
+
+Creates a Shadow object. The object is controled by
+angle (in radians), distance (in px) and strength. Useful values 
+for strength are between 1 and 10.
+Use the created object as parameter for construction of some of the 
+SWF::Filter objects.
 
 =back
 
 =head1 AUTHOR
 
+	developers of 
+	ming.sourceforge.net
 
 =head1 SEE ALSO
 
-SWF, .
+SWF, SWF::Filter, SWF::Blur, SWF::DisplayItem
 
 =cut
