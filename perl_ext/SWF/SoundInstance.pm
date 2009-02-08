eba62ba4761ef2031ef4919692e268e8d9efbe97
akleine <akleine>
2009-02-08 09:47:25 +0000

    added initial pod

--- perl_ext/SWF/SoundInstance.pm
+++ perl_ext/SWF/SoundInstance.pm
@@ -1,5 +1,6 @@
 # ====================================================================
 # Copyright (c) 2000-2001 by Soheil Seyfaie. All rights reserved.
+#           (c) 2009 A.Kleine
 # This program is free software; you can redistribute it and/or modify
 # it under the same terms as Perl itself.
 # ====================================================================
@@ -15,3 +16,74 @@ use strict;
 $SWF::SoundInstance::VERSION = $SWF::VERSION;
 
 1;
+
+__END__
+
+=head1 NAME
+
+SWF::SoundInstance - SWF Sound Instance class
+
+=head1 SYNOPSIS
+
+	use SWF::Sound;
+	use SWF::SoundInstance;
+	$sound = new SWF::Sound("test.mp3");
+	$snd_instance = $movie->startSound($sound);
+	$snd_instance->loopInPoint(4000);
+	$snd_instance->loopOutPoint(8000);
+	$snd_instance->loopCount(2);
+
+=head1 DESCRIPTION
+
+SWF::SoundStream is a helper class useful for setting information
+about a sound instance. There is no constructor method,
+SWF::SoundStream objects are available only thru:
+
+=over
+
+=item startSound() method of SWF::Movie,
+
+=item startSound() method of SWF::MovieClip,
+
+=item addSound()   method of SWF::Button.
+
+=back
+
+=head1 METHODS
+
+=over
+
+=item $snd_instance->noMultiple()
+
+Prohibits starting sound if sound is already playing.
+
+=item $snd_instance->loopInPoint($point)
+
+Sets the loop start $point counted in samples.
+
+=item $snd_instance->loopOutPoint($point)
+
+Sets the loop's last sample to play.
+
+=item $snd_instance->loopCount($count)
+
+Sets loop count. The default value is 1 loop.
+
+=item $snd_instance->addEnvelope($mark44, $left, $right)
+
+Adds a SoundEnvelope to event sound. Parameters are $mark44, the  Position in 44khz samples,
+and volumes both for left and right stereo channel.
+Next example will mute the right channel:
+
+	$snd_instance->addEnvelope(0,10000,0);
+
+=back
+
+=head1 AUTHOR
+
+developers of ming.sourceforge.net, Albrecht Kleine
+
+=head1 SEE ALSO
+
+SWF, SWF:: Movie, SWF::MovieClip, SWF::Button, SWF::Sound, SWF::SoundStream, SWF::Constants
+=cut
--- perl_ext/SWF/SoundStream.pm
+++ perl_ext/SWF/SoundStream.pm
@@ -1,10 +1,12 @@
 # ====================================================================
 # Copyright (c) 2000-2001 by Soheil Seyfaie. All rights reserved.
+#           (c) 2009 A.Kleine
 # This program is free software; you can redistribute it and/or modify
 # it under the same terms as Perl itself.
 # ====================================================================
 
 # $Author$
+# $Id$
 
 package SWF::SoundStream;
 use SWF();
@@ -14,3 +16,56 @@ use strict;
 $SWF::SoundStream::VERSION = $SWF::VERSION;
 
 1;
+
+__END__
+
+=head1 NAME
+
+SWF::SoundStream - SWF Sound Stream class
+
+=head1 SYNOPSIS
+
+	use SWF::SoundStream;
+	my $soundstream = new SWF::SoundStream("wau.mp3"); 
+	print $soundstream->getDuration();
+	##  print $soundstream->getFrames();	# won't work until it belongs to movie
+	$movie->setSoundStream($soundstream);
+	print $soundstream->getFrames();	# but _now_ it is okay
+
+=head1 DESCRIPTION
+
+SWF::SWF::SoundStream is a helper class useful for adding sound into SWF applications.
+
+=head1 METHODS
+
+=over
+
+=item $soundstream = new SWF::SoundStream($filename)
+
+Creates a SWF::SoundStream object. If the file can't be opened the 
+constructor will return an undef value of course. The filename is
+the valid name of any mp3 or flv file.
+
+=item $dura = $soundstream->getDuration()
+
+This function returns the duration of a given stream in ms.
+Works for streams of mp3, flv types only. On other cases it returns 0.
+
+=item $frames = $soundstream->getFrames()
+
+The number of movie frames for a given sound stream This function returns the
+number of movie frames necessary to play the full sound stream. For this reason
+it works only if the sound stream object was added to a movie.
+It works for streams of mp3 -files only.
+
+=back
+
+=head1 AUTHOR
+
+	developers of ming.sourceforge.net, Albrecht Kleine
+
+=head1 SEE ALSO
+
+SWF, SWF:: Movie, SWF::MovieClip, SWF::Button, SWF::Sound, SWF::SoundInstance, SWF::Constants
+
+=cut
