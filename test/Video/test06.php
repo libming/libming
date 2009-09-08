1272db87021a5840d9158ad1d403c19b1b88d5e5
strk <strk>
2009-09-08 08:18:08 +0000

    PHP version of Video test 6

---
+++ test/Video/test06.php
@@ -0,0 +1,32 @@
+#!/usr/bin/php -c.
+<?php
+
+$srcdir = $argv[1];
+
+$m = new SWFMovie(7);
+
+$stream = new SWFVideoStream($srcdir . "/../Media/video01.flv");
+
+$stream->setFrameMode(SWF_VIDEOSTREAM_MODE_MANUAL);
+$stream->setDimension(200, 200);
+
+$frames = $stream->getNumFrames();
+
+$m->add($stream);
+
+$off = 0;
+for($i = 0; $i < 50; $i++)
+{
+	if ( $i == 30 )
+		$off = $stream->seek(50, SEEK_SET);
+
+	if ( $i == 40 )
+		$stream->seek($off, SEEK_SET);
+	
+	$stream->nextFrame();
+	$m->nextFrame();
+}
+
+$m->save("test06.swf");
+
+?>
