a5c51739db364004ca77438d14410ea516f2fca5
strk <strk>
2009-10-09 18:32:26 +0000

    Add PHP version of test 1 for Movie.replace (I wonder why such a complex shape is used in this test, which in turn makes me wonder why arguments in PHP binding are in different order than in C for SWFShape.setLine2)

---
+++ test/Movie/replace/test01.php
@@ -0,0 +1,23 @@
+#!/usr/bin/php -c php.ini
+<?php
+
+$m = new SWFMovie(8);
+
+$shape1 = new SWFShape();
+$shape1->setLine2(1, SWF_LINESTYLE_FLAG_HINTING, 0, 25, 0, 0, 128);
+$shape1->movePenTo(5, 5);
+$shape1->drawLineTo(50, 30);
+
+$shape2 = new SWFShape();
+$shape2->setLine2(1, SWF_LINESTYLE_FLAG_HINTING, 0, 25, 100, 100, 255);
+$shape2->movePenTo(5, 5);
+$shape2->drawLineTo(50, 130);
+
+$item = $m->add($shape1);
+$m->nextFrame();
+$m->replace($item, $shape2);
+$m->nextFrame();
+
+$m->save("test01.swf");
+
+?>
