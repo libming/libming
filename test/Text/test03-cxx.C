22f8d60a2a02a751271bf082a99c5ec0ad46eff1
strk <strk>
2009-10-09 17:22:48 +0000

    Add test for SWFText_moveTo (C and C++)

---
+++ test/Text/test03-cxx.C
@@ -0,0 +1,31 @@
+#include <cstdlib>
+#include <cstdio>
+#include <mingpp.h>
+
+int main()
+{
+	FILE *fontfile;
+	
+	try {
+		SWFMovie *m = new SWFMovie();
+		SWFText *text = new SWFText(2);
+	
+		SWFFont *font = new SWFFont(MEDIADIR "/font01.fdb");
+	
+		text->setFont( font);
+		text->setColor( 0, 0, 0, 0xff);
+		text->setHeight( 20);
+		text->moveTo(10, 20);
+		text->addString( "abc", NULL);
+		m->add(text);
+		m->nextFrame();
+		m->save("test03.swf");
+	}
+	catch(SWFException &e)
+	{
+		std::cerr << "SWFException: " << e.what() << std::endl << std::endl;
+		return EXIT_FAILURE;
+	}
+	return 0;
+}
+	
---
+++ test/Text/test03.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <libming.h>
+
+int main()
+{
+	FILE *fontfile;
+	
+	SWFMovie m = newSWFMovie();
+	SWFText text = newSWFText2();
+	fontfile = fopen(MEDIADIR "/font01.fdb", "rb");
+	if(fontfile == NULL)
+	{
+		perror(MEDIADIR "/font01.fdb");
+		exit(EXIT_FAILURE);
+	}
+	
+	SWFFont font = loadSWFFontFromFile(fontfile);
+	if(font == NULL)
+	{
+		fprintf(stderr, "Could not load SWFFont from file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	
+	SWFText_setFont(text, font);
+	SWFText_setColor(text, 0, 0, 0, 0xff);
+	SWFText_setHeight(text, 20);
+	SWFText_moveTo(text, 10, 20);
+	SWFText_addString(text, "abc", NULL);
+
+	SWFMovie_add(m, text);
+	SWFMovie_nextFrame(m);
+	SWFMovie_save(m, "test03.swf");
+
+	return 0;
+}
+	
