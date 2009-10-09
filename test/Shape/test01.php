8003d61d089e3e3c6c68284542cac8959a1bdbc4
strk <strk>
2009-10-09 21:28:48 +0000

    PHP version of Shape tests

---
+++ test/Shape/test01.php
@@ -0,0 +1,16 @@
+#!/usr/bin/php -c php.ini
+<?php
+
+$m = new SWFMovie(8);
+
+$shape1 = new SWFShape();
+$shape2 = new SWFShape();
+
+$shape2->useVersion(SWF_SHAPE4);
+
+
+$m->add($shape1);
+$m->add($shape2);
+
+$m->save("test01.swf");
+?>
