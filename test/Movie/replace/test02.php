7e2ecb9a555c9da94c2f240075e550e1ffcee442
strk <strk>
2009-10-09 18:34:04 +0000

    Second php test

---
+++ test/Movie/replace/test02.php
@@ -0,0 +1,18 @@
+#!/usr/bin/php -c php.ini
+<?php
+
+$m = new SWFMovie(8);
+
+$shape1 = new SWFShape();
+$shape1->setLine2(1, SWF_LINESTYLE_FLAG_HINTING, 0, 25, 0, 0, 128);
+$shape1->movePenTo(5, 5);
+$shape1->drawLineTo(50, 30);
+
+$item = $m->add($shape1);
+$m->nextFrame();
+$m->replace($item, $shape1);
+$m->nextFrame();
+
+$m->save("test02.swf");
+
+?>
