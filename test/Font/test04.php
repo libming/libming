2f7225abbe35df381900b187d1b5fb4c9be1c743
strk <strk>
2009-12-01 21:02:19 +0000

    Ehm.. forgot to add the actual test

---
+++ test/Font/test04.php
@@ -0,0 +1,32 @@
+#!/usr/bin/php -c php.ini
+
+<?php
+
+$srcdir = $argv[1];
+$mediadir = $srcdir . "/../Media";
+
+$m = new SWFMovie();
+
+$f = new SWFFont($mediadir . "/font01.fdb");
+
+$t = new SWFText(1);
+$t->setFont($f);
+$t->setHeight(20);
+$t->setColor(0x00, 0x00, 0xff);
+$t->moveTo(100, 100);
+$t->addString("1234567890");
+
+$tf = new SWFTextField(1);
+$tf->setFont($f);
+$tf->setHeight(20);
+$tf->setColor(0xff, 0x00, 0x00);
+$tf->addString("1234567890");
+
+$m->add($t);
+$it = $m->add($tf);
+$it->moveTo(100, 120);
+
+$m->nextFrame(); /* end of frame 1 */
+
+$m->save("test04.swf");
+?>
