fb8f0fa3851e05a23216ff00a189e030283bb252
strk <strk>
2009-12-03 18:55:26 +0000

    Add PHP version of all tests in TextField suite

---
+++ test/TextField/test01.php
@@ -0,0 +1,22 @@
+#!/usr/bin/php -c php.ini
+
+<?php
+
+$srcdir = $argv[1];
+$mediadir = $srcdir . "/../Media";
+
+$m = new SWFMovie();
+
+$f = new SWFFont($mediadir . "/test.ttf");
+
+$tf = new SWFTextField();
+$tf->setFont($f);
+$tf->setColor(0x0, 0x00, 0x00, 0xff);
+$tf->setHeight(20);
+$tf->setFlags(SWFTEXTFIELD_NOEDIT);
+$tf->addString("The quick brown fox jumps over the lazy dog. 1234567890");
+
+$m->add($tf);
+$m->nextFrame(); /* end of frame 1 */
+$m->save("test01.swf");
+?>
---
+++ test/TextField/test02.php
@@ -0,0 +1,20 @@
+#!/usr/bin/php -c php.ini
+
+<?php
+
+$srcdir = $argv[1];
+$mediadir = $srcdir . "/../Media";
+
+$m = new SWFMovie();
+
+$f = new SWFFont($mediadir . "/test.ttf");
+
+$tf = new SWFTextField();
+$tf->setFont($f);
+$tf->setColor(0x0, 0x00, 0x00, 0xff);
+$tf->setHeight(20);
+
+$m->add($tf);
+$m->nextFrame(); /* end of frame 1 */
+$m->save("test02.swf");
+?>
