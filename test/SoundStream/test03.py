00aa9489dd1dde841e6e7e560bd6fca5317fd21b
Sandro Santilli <strk@keybit.net>
2011-03-15 17:13:26 +0100

    Add python version of SWFSoundStream/test03, and fix test02 to be 1:1 with C version, now that getRate is fixed

--- test/SoundStream/test03.py
+++ test/SoundStream/test03.py
@@ -7,14 +7,17 @@ mediadir = sys.argv[1]+'/../Media'
 Ming_useSWFVersion(7);
 m =  SWFMovie();
 
-mc =  SWFMovieClip(); 
+mc = SWFMovieClip(); 
 
 stream = SWFSoundStream(mediadir + '/audio01.mp3')
+stream.setInitialMp3Delay(16384);
+
 mc.setSoundStream(stream, m.getRate())
 for i in range (0, 200):
 	mc.nextFrame()
 
-m.add(mc);
-m.nextFrame(); 
 
-m.save("test02.swf");
+
+m.add(mc);
+
+m.save("test03.swf");
