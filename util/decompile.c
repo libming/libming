0c6fafc682a3b7b0c94035d15c56d3d16d5ea5e3
Sandro Santilli <strk@keybit.net>
2011-03-19 14:36:14 +0100

    Use ASM for extend op (easiest way)

--- util/decompile.c
+++ util/decompile.c
@@ -3033,11 +3033,24 @@ decompileEXTENDS(int n, SWF_ACTION *actions, int maxn)
 	struct SWF_ACTIONPUSHPARAM *baseclass;
 
 	baseclass=pop();
+#if 0
+	/* It's useless to open a class body when there's no
+	 * other code supporting it. */
 	printf("class ");
 	puts(getName(pop()));
 	printf(" extends ");
 	puts(getName(baseclass));
 	println(" {" );
+#else
+	/* We'll do it with asm{} */
+	println("asm {");
+	println(" push '%s'", getName(pop()));
+	println(" getvariable");
+	println(" push '%s'", getName(baseclass));
+	println(" getvariable");
+	println(" extends");
+	println("};");
+#endif
 
 	return 0;
 }
