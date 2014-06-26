85dcf3da54c84ddd1ae9d368467cbe1404be2dbe
Sandro Santilli <strk@keybit.net>
2014-06-26 19:06:24 +0200

    Change licence of PHP wrapper back to LGPL-2.1+
    This is a revival of a8bef56
    
    Still reachable authors gave their approval here:
    http://sourceforge.net/p/ming/mailman/message/32503963/
    
    The change was needed to prevent Debian from moving the php wrapper
    in the "nonfree" repository, and to fix an incompatibility in binary
    packages.
    
    Closes #42

--- php_ext/ming.c
+++ php_ext/ming.c
@@ -1,25 +1,27 @@
 /*
-  +----------------------------------------------------------------------+
-  | PHP Version 5                                                        |
-  +----------------------------------------------------------------------+
-  | Copyright (c) 1997-2008 The PHP Group                                |
-  +----------------------------------------------------------------------+
-  | This source file is subject to version 3.01 of the PHP license,      |
-  | that is bundled with this package in the file LICENSE, and is        |
-  | available through the world-wide-web at the following url:           |
-  | http://www.php.net/license/3_01.txt                                  |
-  | If you did not receive a copy of the PHP license and are unable to   |
-  | obtain it through the world-wide-web, please send a note to          |
-  | license@php.net so we can mail you a copy immediately.               |
-  +----------------------------------------------------------------------+
-  | Authors: Dave Hayden <dave@opaque.net>                               |
-  |          Frank M. Kromann <fmk@php.net>                              |
-  |          Stuart R. Anderson <anderson@netsweng.com>                  |
-  |          Klaus Rechert <klaus@rechert.de>                            |
-  +----------------------------------------------------------------------+
-*/
+  Ming, an SWF output library
 
-/* $Id$ */
+  Copyright (C) 2004-2011 Sandro Santilli <strk@keybit.net>
+  Copyright (C) 2006-2013 Stuart R. Anderson <anderson@netsweng.com>
+  Copyright (C) 2004-2008 Klaus Rechert <klaus@rechert.de>
+  Copyright (C) 2008      Frank M. Kromann <fmk@php.net>
+  Copyright (C) 2001-2002 Dave Hayden <dave@opaque.net>
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
