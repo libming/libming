libming/libming

commit d468907a46a7ca42a78b0ac6f221172905be2fd6

Fix readSInt16, readUInt16 and readSInt32 methods
* Rewrite readSInt16, readUInt16 and readSInt32.

    Avoid calling all read{U,S}Int8(f) in one line, order of
    evaluation is not guaranteed in the C standard (undefined
    behavior).

* Change return type of readUInt16 from int to unsigned int.

* Rewrite readUInt32.

    Use |= operator instead of adding four separate integers. Less
    memory usage, better readable.

util/read.c
@@ -148,26 +148,38 @@ int readSInt8(FILE *f)
 
 int readSInt16(FILE *f)
 {
-  return readUInt8(f) + readSInt8(f)*256;
+  int result = 0;
+  result |= readUInt8(f);
+  result |= readSInt8(f) << 8;
+  return result;
 }
 
-int readUInt16(FILE *f)
+unsigned int readUInt16(FILE *f)
 {
-  return readUInt8(f) + (readUInt8(f)<<8);
+  unsigned int result = 0u;
+  result |= readUInt8(f);
+  result |= readUInt8(f) << 8;
+  return result;
 }
 
 long readSInt32(FILE *f)
 {
-  return (long)readUInt8(f) + (readUInt8(f)<<8) + (readUInt8(f)<<16) + (readUInt8(f)<<24);
+  long result = 0;
+  result |= readUInt8(f);
+  result |= readUInt8(f) << 8;
+  result |= readUInt8(f) << 16;
+  result |= readUInt8(f) << 24;
+  return result;
 }
 
 unsigned long readUInt32(FILE *f)
 {
-  int part1 = readUInt8(f);
-  int part2 = readUInt8(f) << 8;
-  int part3 = readUInt8(f) << 16;
-  unsigned long part4 = ((unsigned long)readUInt8(f)) << 24;
-  return part1 + part2 + part3 + part4;
+  unsigned long result = 0u;
+  result |= readUInt8(f);
+  result |= readUInt8(f) << 8;
+  result |= readUInt8(f) << 16;
+  result |= readUInt8(f) << 24;
+  return result;
 }
 
 double readDouble(FILE *f)

util/read.h
@@ -13,7 +13,7 @@ int readBits(FILE *f, int number);
 int readSBits(FILE *f, int number);
 int readUInt8(FILE *f);
 int readSInt8(FILE *f);
-int readUInt16(FILE *f);
+unsigned int readUInt16(FILE *f);
 int readSInt16(FILE *f);
 unsigned long readUInt32(FILE *f);
 long readSInt32(FILE *f);

commit c14d07ef20c3f403fcfa59502b74c66933473431

Fix integer overflow vulnerability in util/read.c.
This vulnerability is caused by a regression introduced in
d468907.

In this commit we cast the result of readUInt8(f) before left
shifting by 24 in order to avoid out of range shift.

This commit fixes CVE-2018-5251 (fixes #98).

util/read.c
@@ -168,7 +168,7 @@ long readSInt32(FILE *f)
   result |= readUInt8(f);
   result |= readUInt8(f) << 8;
   result |= readUInt8(f) << 16;
-  result |= readUInt8(f) << 24;
+  result |= (long) readUInt8(f) << 24;
   return result;
 }
 
@@ -178,7 +178,7 @@ unsigned long readUInt32(FILE *f)
   result |= readUInt8(f);
   result |= readUInt8(f) << 8;
   result |= readUInt8(f) << 16;
-  result |= readUInt8(f) << 24;
+  result |= (unsigned long) readUInt8(f) << 24;
   return result;
 }
 