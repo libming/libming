2223f7a1e431455a1411bee77c90db94a6f8e8fe
Young Xiao <YangX92@hotmail.com>
2019-03-16 18:50:24 +0800

    Fix left shift of a negative value in SWFInput_readSBits. Check for number before before left-shifting by (number-1).

--- src/blocks/input.c
+++ src/blocks/input.c
@@ -113,7 +113,7 @@ SWFInput_readSBits(SWFInput input, int number)
 {
 	int num = SWFInput_readBits(input, number);
 
-	if ( num & (1<<(number-1)) )
+	if(number && num & (1<<(number-1)))
 		return num - (1<<number);
 	else
 		return num;
